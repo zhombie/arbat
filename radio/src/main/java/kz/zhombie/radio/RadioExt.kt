d12fc3367a99bc1a67575371517c90bf49029eb2
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2021-04-03 01:47:50 +0500

    [Radio] Time

--- radio/src/main/java/kz/zhombie/radio/RadioExt.kt
+++ radio/src/main/java/kz/zhombie/radio/RadioExt.kt
@@ -5,13 +5,18 @@ import java.util.concurrent.TimeUnit
 fun Radio.formatToDigitalClock(milliseconds: Long?): String {
     if (isReleased()) return "00:00"
     if (milliseconds == null) return "00:00"
-    val hours = TimeUnit.MILLISECONDS.toHours(milliseconds).toInt() % 24
-    val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds).toInt() % 60
-    val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds).toInt() % 60
-    return when {
-        hours > 0 -> String.format("%d:%02d:%02d", hours, minutes, seconds)
-        minutes > 0 -> String.format("%02d:%02d", minutes, seconds)
-        seconds > 0 -> String.format("00:%02d", seconds)
-        else -> "00:00"
+    return try {
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds).toInt() % 24
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds).toInt() % 60
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds).toInt() % 60
+        when {
+            hours > 0 -> String.format("%d:%02d:%02d", hours, minutes, seconds)
+            minutes > 0 -> String.format("%02d:%02d", minutes, seconds)
+            seconds > 0 -> String.format("00:%02d", seconds)
+            else -> "00:00"
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return "00:00"
     }
 }
